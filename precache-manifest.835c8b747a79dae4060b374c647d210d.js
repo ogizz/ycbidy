e31ecca4c85aba2431866c92c48e8185a6764d3a
ogizz <shubh270795@gmail.com>
2020-08-03 23:48:29 +0530

    new0 commit

---
+++ precache-manifest.835c8b747a79dae4060b374c647d210d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a2b1a789f1bfa615209e8fff263a88",
+    "url": "/ycbidy/index.html"
+  },
+  {
+    "revision": "26d1253084847cfb1b3f",
+    "url": "/ycbidy/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "c89daf47af39d27e28ce",
+    "url": "/ycbidy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "26d1253084847cfb1b3f",
+    "url": "/ycbidy/static/js/2.a3a50c9a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ycbidy/static/js/2.a3a50c9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c89daf47af39d27e28ce",
+    "url": "/ycbidy/static/js/main.7be407d9.chunk.js"
+  },
+  {
+    "revision": "80d81d749544ba8c188d",
+    "url": "/ycbidy/static/js/runtime-main.ee8ccf87.js"
+  },
+  {
+    "revision": "c706d02cb0f213e7f028128564c49dcf",
+    "url": "/ycbidy/static/media/idy.c706d02c.png"
+  },
+  {
+    "revision": "c5e5148a2191b52498c3f4883b448e98",
+    "url": "/ycbidy/static/media/slide1.c5e5148a.jpg"
+  },
+  {
+    "revision": "6b68f0f3ca677235a7d6f0f6f5f70c21",
+    "url": "/ycbidy/static/media/slide2.6b68f0f3.jpg"
+  },
+  {
+    "revision": "7c8284eb7b1ea692f103c058c56cf8ef",
+    "url": "/ycbidy/static/media/slide3.7c8284eb.jpg"
+  },
+  {
+    "revision": "97d9c80fd5de5f59da1efbcd488b8660",
+    "url": "/ycbidy/static/media/slide4.97d9c80f.jpg"
+  },
+  {
+    "revision": "b5cfa9e84a79eb356d1cd39338845002",
+    "url": "/ycbidy/static/media/slide5.b5cfa9e8.jpg"
+  },
+  {
+    "revision": "59a683a24df3cd531f45baa83cadc28f",
+    "url": "/ycbidy/static/media/ycb.59a683a2.png"
+  },
+  {
+    "revision": "dc6da0e393fd75212bead18177188880",
+    "url": "/ycbidy/static/media/ycb2.dc6da0e3.png"
+  }
+]);
